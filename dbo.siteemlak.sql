c82c79a91d389f4c1085276f67d8df1eeaa5a54a
kaan00mkt <106037437+kaan00mkt@users.noreply.github.com>
2022-05-23 01:12:28 +0300

    Add files via upload

---
+++ dbo.siteemlak.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [site] NCHAR(10) NULL, 
+    [satkira] NCHAR(10) NULL, 
+    [oda] NCHAR(10) NULL, 
+    [metre] NCHAR(10) NULL, 
+    [fiyat] NCHAR(10) NULL, 
+    [blok] NCHAR(10) NULL, 
+    [no] NCHAR(10) NULL, 
+    [adsoyad] NCHAR(10) NULL, 
+    [telefon] NCHAR(10) NULL, 
+    [notlar] NCHAR(200) NULL
+)
